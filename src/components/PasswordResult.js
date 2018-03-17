599a340a32804615055d9e98d92bc430bcf7e37c
Jonas Kay <jonas-kay@outlook.com>
2018-03-17 16:51:59 +0200

    Text inside result input field is selected after password generation

--- src/components/PasswordResult.js
+++ src/components/PasswordResult.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import './PasswordResult.css';
 
 class PasswordResult extends Component {
+  constructor(props) {
+    super(props);
+    this.selectTextInput = this.selectTextInput.bind(this);
+  }
+
+  selectTextInput() {
+    this.textInput.select();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.result !== this.props.result) {
+      this.selectTextInput();
+    }
+  }
+
   render() {
     return (
       <div className="PasswordResult">
-        <input type="text" value={this.props.result} />
+        <input type="text" value={this.props.result} ref={(input) => { this.textInput = input; }} />
       </div>
     );
   }
