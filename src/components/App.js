86561e4f55c485e37ab3f8be36b7e6428e76e968
jonaskay <jonaskay@iki.fi>
2019-12-08 16:08:11 +0200

    Update footer

--- src/components/App.js
+++ src/components/App.js
@@ -48,7 +48,7 @@ class App extends Component {
         <PasswordResult result={this.state.result} />
         <footer className="App-footer">
           <span>
-            &copy; 2018 <a href="https://github.com/jonaskay">Jonas Kay</a>
+            &copy; 2019 <a href="https://github.com/jonaskay">jonaskay</a>
           </span>
           <span style={{ textAlign: 'right' }}>
             Based on <a href="https://crypto.stanford.edu/PwdHash/">PwdHash</a>
